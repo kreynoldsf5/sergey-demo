fb91631ea3fa1cdf899b3aa93da2bc1f6d1109a6
kreynoldsf5 <k.reynolds@f5.com>
2022-04-22 14:28:12 -0400

    added output

--- infra.tf
+++ infra.tf
@@ -89,3 +89,8 @@ resource "volterra_http_loadbalancer" "lb" {
   no_challenge                    = true
   add_location                    = true
 }
+
+output "app_url" {
+  description = "Domain VIP to access the web app"
+  value       = format("https://%s", var.app_fqdn)
+}
